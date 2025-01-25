60f744e8e5d1a401cb19c20a9be79d3e3717a74b
Mazen <99512078+Prossle@users.noreply.github.com>
2025-01-25 10:40:54 +0200

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,28 @@
+//1
+
+let NumberOne = 10, NumberTwo = 20;
+
+console.log(NumberOne + "" + NumberTwo);
+console.log(typeof (NumberOne + "" + NumberTwo));
+console.log(`${NumberOne}${NumberTwo}`);
+console.log(`${typeof (`${NumberOne}${NumberTwo}`)}`);
+console.log(NumberTwo + "\n" + NumberOne);
+console.log(`${NumberTwo}
+${NumberOne}`);
+
+
+//2
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+//3
+
+console.log("`I'm In\n\\\\\nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+
+//4
+
+let a = 21;
+let b = 20;
+
+console.log(`_${a}_${`${b}${a}_`.repeat(3)}${b}_`);
